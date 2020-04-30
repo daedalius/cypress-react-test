91a573218ce80060d62e261a5f0d1ca10fb61323
Dmitriy <daedaliusx@gmail.com>
2020-04-30 23:24:03 +0400

    added another copy on Datepicker test

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,7 +1,2 @@
 
 require('cypress-react-unit-test/support')
-import '@testing-library/cypress/add-commands'
-import './commands'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
